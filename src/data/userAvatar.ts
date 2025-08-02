c2a857f5593889f27b3fd1bbb5661c689093e04d
Olena457 <olenademianenko42@gmail.com>
2025-08-03 02:12:19 +0300

    added photo

---
+++ src/data/userAvatar.ts
@@ -0,0 +1,10 @@
+const userImages: string[] = [
+  "https://ftp.goit.study/img/avatars/2.jpg",
+  "https://ftp.goit.study/img/avatars/3.jpg",
+  "https://ftp.goit.study/img/avatars/4.jpg",
+  "https://ftp.goit.study/img/avatars/5.jpg",
+  "https://ftp.goit.study/img/avatars/7.jpg",
+  "https://ftp.goit.study/img/avatars/9.jpg",
+];
+
+export default userImages;
