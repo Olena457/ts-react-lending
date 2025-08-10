e8672cdfdf1e51a0f74ce07d2fe3273abc71e87f
Olena457 <olenademianenko42@gmail.com>
2025-08-10 21:17:03 +0300

    Update blogsData.ts

--- src/api/blogsData.ts
+++ src/api/blogsData.ts
@@ -1,66 +1,7 @@
-// import type { IBlogList } from "../types/IBlogList";
-
-// export const blogsData: IBlogList[] = [
-//   {
-//     id: 1,
-//     img: "../assets/img/projects/apartment.jpg",
-//     title: "Modern Residential Design: Inspiration for Private Homes",
-//     date: 17,
-//     month: "November",
-//     year: 2023,
-//     category: "living room",
-//   },
-//   {
-//     id: 2,
-//     img: "../assets/img/projects/kitchen-3.jpg",
-//     title: "Functional Kitchen: Where Style Meets Convenience",
-//     date: 22,
-//     month: "September",
-//     year: 2024,
-//     category: "Kitchen Interior",
-//   },
-//   {
-//     id: 3,
-//     img: "../assets/img/projects/bedroom.jpg",
-//     title: "Cozy Bedroom: Creating a Space for Rest and Relaxation",
-//     date: 15,
-//     month: "August",
-//     year: 2024,
-//     category: "Bedroom",
-//   },
-//   {
-//     id: 4,
-//     img: "../assets/img/projects/kitchen.jpg",
-//     title: "Apartment Kitchen Interior: Compact and Stylish Solutions",
-//     date: 27,
-//     month: "August",
-//     year: 2024,
-//     category: "Apartment",
-//   },
-//   {
-//     id: 5,
-//     img: "../assets/img/projects/business.jpg",
-//     title: "Café Interior: Crafting an Inviting Atmosphere for Guests",
-//     date: 12,
-//     month: "June",
-//     year: 2025,
-//     category: "Commercial Space",
-//   },
-//   {
-//     id: 6,
-//     img: "../assets/img/projects/business-2.jpg",
-//     title: "Office Style: Designing a Professional Meeting Room",
-//     date: 4,
-//     month: "May",
-//     year: 2025,
-//     category: "Commercial Space",
-//   },
-// ];
-
 import type { IBlogList } from "../types/IBlogList";
 import apartmentImg from "../assets/img/projects/apartment.jpg";
 import kitchen3Img from "../assets/img/projects/kitchen-3.jpg";
-import bedroomImg from "../assets/img/projects/bedroom.jpg";
+import bedroom1Img from "../assets/img/projects/bedroom-1.jpg";
 import kitchenImg from "../assets/img/projects/kitchen.jpg";
 import businessImg from "../assets/img/projects/business.jpg";
 import business2Img from "../assets/img/projects/business-2.jpg";
@@ -86,7 +27,7 @@ export const blogsData: IBlogList[] = [
   },
   {
     id: 3,
-    img: bedroomImg,
+    img: bedroom1Img,
     title: "Cozy Bedroom: Creating a Space for Rest and Relaxation",
     date: 15,
     month: "August",
