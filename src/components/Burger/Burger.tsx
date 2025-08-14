fb6ad1e1204cd7746e28869aa913dc6f27d0ab0f
Olena457 <olenademianenko42@gmail.com>
2025-08-14 22:55:21 +0300

    fixed burger menu

--- src/components/Burger/Burger.tsx
+++ src/components/Burger/Burger.tsx
@@ -24,7 +24,7 @@ const Burger: React.FC<Props> = ({ isOpen, onClick }) => {
 
   const showCategories = mainCategories.map((item) => (
     <ul key={item.id}>
-      <a href={item.link} onClick={onClick} className={styles.linkBurger}>
+      <a href={`#${item.link}`} onClick={onClick} className={styles.linkBurger}>
         {item.category}
       </a>
     </ul>
